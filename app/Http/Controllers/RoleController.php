c824b516e68f544687c91d10f6c0d70b1d45b715
MdSafaet <mdsafet84@gmail.com>
2024-11-05 11:36:28 +0600

    Product part still not done

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
@@ -12,7 +14,10 @@ class RoleController extends Controller
      */
     public function index()
     {
-        //
+        $roles =Role::orderBy('created_at','DESC')->paginate(10);
+        return view('roles.list',[
+            'roles' => $roles
+        ]);
     }
 
     /**
@@ -31,23 +36,43 @@ class RoleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|unique:roles,name'
+        ]);
+
+        if ($validator->passes()) {
+            $role = Role::create(['name' => $request->name]);
+
+            if (!empty($request->permission)) {
+                foreach ($request->permission as $name) {
+                    $role->givePermissionTo($name);
+                }
+            }
+
+            return redirect()->route('roles.index')->with('success', 'Roles Created Successfully');
+        } else {
+            return redirect()->route('roles.create')->withInput()->withErrors($validator);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
-    {
-        //
-    }
+
 
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(string $id)
     {
-        //
+            $role = Role::findorFail($id);
+            $permissions = Permission::OrderBy('name','DESC')->get();
+            $hasPermission = $role->permissions->pluck('name');
+            return view('roles.edit',[
+                'role' => $role,
+                'permissions' => $permissions,
+                'hasPermission' => $hasPermission
+            ]);
     }
 
     /**
@@ -55,14 +80,43 @@ class RoleController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
-    }
+
+        $role = Role::findOrFail($id);
+
+        $validator = validator::make($request->all(),[
+            'name' => 'required|unique:roles,name,'.$id.',id'
+        ]);
+
+        if($validator->passes()){
+
+            $role->name=$request->name;
+            $role->save();
+
+            if (!empty($request->permission)) {
+                  $role->syncPermissions($request->permission);
+                }
+              else{
+                    $role->syncPermissions([]);
+                }
+
+
+            return redirect()->route('roles.index')->with('success', 'Roles Updated Successfully');
+            }
+            else{
+                return redirect()->route('roles.edit',$id)->withInput()->withErrors($validator);
+
+            }
+        }
+
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $role=Role::findOrFail($id);
+            $role->delete();
+            return redirect()->route('roles.index')->with('delete');
     }
 }
